05462343c22e6bed0a97d437520ec2be880d1194
vasshorin <)tSz%gBWz1>
2023-05-09 19:04:29 -0700

    Add the expenses table

--- src/components/expenses/CreateNewExpense.js
+++ src/components/expenses/CreateNewExpense.js
@@ -1,41 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const CreateNewExpense = () => {
   const [date, setDate] = useState('');
-  const [comment, setComment] = useState('');
+  const [description, setdescription] = useState('');
   const [amount, setAmount] = useState('');
-  const [category, setCategory] = useState('');
-  const [account, setAccount] = useState('');
-  const [importance, setImportance] = useState('');
+  const [categories, setcategories] = useState('');
+  const [accounts, setaccounts] = useState('');
+  const [type, settype] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    const res = axios.post('/api/newExpense', {
-      date: date,
-      comment: comment,
+    const savedUser = localStorage.getItem('user');
+    if (!savedUser) {
+      // handle user not logged in
+      return;
+    }
+    const userId = JSON.parse(savedUser)._id;
+    const res = await axios.post('http://localhost:5050/api/newTransaction', {
+      userId: userId,
+      type: type,
       amount: amount,
-      category: category,
-      account: account,
-      importance: importance
+      date: date,
+      description: description,
+      categories: categories,
+      accounts: accounts,
     })
-    console.log({ date, comment, amount, category, account, importance });
+    console.log({ date, description, amount, categories, accounts, type });
     // get the response from the server
     console.log(res);
     // clear the form
     setDate('');
-    setComment('');
+    setdescription('');
     setAmount('');
-    setCategory('');
-    setAccount('');
-    setImportance('');
-    
+    setcategories('');
+    setaccounts('');
+    settype('');
   };
+  
 
   return (
     <div className="flex justify-center">
     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
     <div className="flex flex-wrap justify-end">
+    <div className="w-full md:w-1/6 mb-4 md:mb-0">
+      <label className="block text-gray-700 font-bold mb-2" htmlFor="type">
+        type
+      </label>
+      <select
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="type"
+        value={type}
+        onChange={(e) => settype(e.target.value)}
+      >
+        <option value="">Select an type level</option>
+        <option value="expense">expense</option>
+        <option value="income">income</option>
+      </select>
+    </div>
     <div className="w-full md:w-1/6 mb-4 md:mb-0">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
             Date
@@ -50,16 +72,16 @@ const CreateNewExpense = () => {
           />
         </div>
         <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="comment">
-            Comment
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
+            description
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="comment"
+            id="description"
             type="text"
-            placeholder="Enter a comment"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
+            placeholder="Enter a description"
+            value={description}
+            onChange={(e) => setdescription(e.target.value)}
           />
         </div>
         <div className="w-full md:w-1/6 mb-4 md:mb-0">
@@ -77,17 +99,17 @@ const CreateNewExpense = () => {
           />
         </div>
         <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="category">
-            Category
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="categories">
+            categories
           </label>
           <select
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            id="categories"
+            value={categories}
+            onChange={(e) => setcategories(e.target.value)}
           >
-            <option value="">Select a category</option>
-            <option value="food">Food</option>
+            <option value="">Select a categories</option>
+            <option value="food">food</option>
             <option value="transportation">Transportation</option>
             <option value="utilities">Utilities</option>
             <option        value="entertainment">Entertainment</option>
@@ -97,33 +119,17 @@ const CreateNewExpense = () => {
       </select>
     </div>
     <div className="w-full md:w-1/6 mb-4 md:mb-0">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="account">
-        Account
+      <label className="block text-gray-700 font-bold mb-2" htmlFor="accounts">
+        accounts
       </label>
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="account"
+        id="accounts"
         type="text"
-        placeholder="Enter the account"
-        value={account}
-        onChange={(e) => setAccount(e.target.value)}
+        placeholder="Enter the accounts"
+        value={accounts}
+        onChange={(e) => setaccounts(e.target.value)}
       />
-    </div>
-    <div className="w-full md:w-1/6 mb-4 md:mb-0">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="importance">
-        Importance
-      </label>
-      <select
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="importance"
-        value={importance}
-        onChange={(e) => setImportance(e.target.value)}
-      >
-        <option value="">Select an importance level</option>
-        <option value="high">High</option>
-        <option value="medium">Medium</option>
-        <option value="low">Low</option>
-      </select>
     </div>
     <div className="w-full md:w-1/6 mb-4 md:mb-0 flex items-center justify-center">
       <button
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -4,44 +4,74 @@ import axios from 'axios';
 
 const Expenses = () => {
   const [expenses, setExpenses] = useState([]);
+  const [accessToken, setAccessToken] = useState('');
+  const [user, setUser] = useState({});
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('http://localhost:5050/expenses');
-      setExpenses(response.data);
+    const fetchExpenses = async () => {
+      const token = localStorage.getItem('refreshToken');
+      const user = localStorage.getItem('user');
+      const response = await axios.get(`http://localhost:5050/api/transactions`, {
+        headers: { 'auth-token-refresh': token },
+      });
+      setExpenses(response.data.transactions);
     };
-    fetchData();
+    fetchExpenses();
   }, []);
+
+  const onLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('user');
+    setAccessToken('');
+    setUser({});
+  };
 
   return (
     <div>
-        <h1>Add expense</h1>
-        <CreateNewExpense />
+      <CreateNewExpense />
       <h1 className="text-2xl font-bold mb-4">Expenses</h1>
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Date</th>
-            <th className="px-4 py-2">Comment</th>
-            <th className="px-4 py-2">Amount</th>
-            <th className="px-4 py-2">Category</th>
-            <th className="px-4 py-2">Account</th>
-            <th className="px-4 py-2">Importance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense) => (
-            <tr key={expense._id}>
-              <td className="border px-4 py-2">{expense.date}</td>
-              <td className="border px-4 py-2">{expense.comment}</td>
-              <td className="border px-4 py-2">{expense.amount}</td>
-              <td className="border px-4 py-2">{expense.category}</td>
-              <td className="border px-4 py-2">{expense.account}</td>
-              <td className="border px-4 py-2">{expense.importance}</td>
+      {expenses.length > 0 ? (
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Type</th>
+              <th className="px-4 py-2">Amount</th>
+              <th className="px-4 py-2">Date</th>
+              <th className="px-4 py-2">Description</th>
+              <th className="px-4 py-2">Categories</th>
+              <th className="px-4 py-2">Accounts</th>
+              <th className="px-4 py-2">Importance</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={expense._id}>
+                <td className="border px-4 py-2">{expense.type}</td>
+                <td className="border px-4 py-2">{expense.amount}</td>
+                <td className="border px-4 py-2">{expense.date}</td>
+                <td className="border px-4 py-2">{expense.description}</td>
+                <td className="border px-4 py-2">{expense.categories}</td>
+                <td className="border px-4 py-2">{expense.accounts}</td>
+                <td className="border px-4 py-2">{expense.importance}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No expenses found.</p>
+      )}
+
+      {/* Add logout */}
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={(onLogout) => {
+          localStorage.removeItem('user');
+          localStorage.removeItem('refreshToken');
+          window.location.reload();
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
