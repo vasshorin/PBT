de0de4f283ca850fe093fe1b844b636bc4bb124e
vasshorin <)tSz%gBWz1>
2023-05-11 16:40:21 -0700

    Implemnet transaction deletion

--- src/components/expenses/CreateNewExpense.js
+++ src/components/expenses/CreateNewExpense.js
@@ -40,7 +40,7 @@ const CreateNewExpense = () => {
     };
     handleGetCategories();
     handleGetAccounts();
-    }, [refreshToken]);
+  }, [refreshToken]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -70,115 +70,115 @@ const CreateNewExpense = () => {
     setaccounts('');
     settype('');
   };
-  
+
 
   return (
     <div className="flex justify-center">
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
-    <div className="flex flex-wrap justify-end">
-    <div className="w-full md:w-1/6 mb-4 md:mb-0">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="type">
-        type
-      </label>
-      <select
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="type"
-        value={type}
-        onChange={(e) => settype(e.target.value)}
-      >
-        <option value="">Select an type level</option>
-        <option value="expense">expense</option>
-        <option value="income">income</option>
-      </select>
-    </div>
-    <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
-            Date
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="date"
-            type="date"
-            placeholder="Enter the date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-        <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
-            description
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="description"
-            type="text"
-            placeholder="Enter a description"
-            value={description}
-            onChange={(e) => setdescription(e.target.value)}
-          />
-        </div>
-        <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="amount">
-            Amount
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="amount"
-            type="number"
-            step=".01"
-            placeholder="Enter the amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
-        <div className="w-full md:w-1/6 mb-4 md:mb-0">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="categories">
-            categories
-          </label>
-          <select
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="categories"
-            value={categories}
-            onChange={(e) => setcategories(e.target.value)}
-          >
-            <option value="">Select an category level</option>
-            {Array.isArray(categories) && categories.map((category) => (
-  <option key={category._id} value={category._id}>
-    {category}
-  </option>
-))}
-      </select>
-    </div>
-    <div className="w-full md:w-1/6 mb-4 md:mb-0">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="accounts">
-        accounts
-      </label>
-      <select
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="accounts"
-        value={accounts}
-        onChange={(e) => setaccounts(e.target.value)}
-      >
-        <option value="">Select an account level</option>
-        {Array.isArray(accounts) && accounts.map((account) => (
-  <option key={account._id} value={account._id}>
-    {account.name}
-  </option>
-))}
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+        <div className="flex flex-wrap justify-end">
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="type">
+              type
+            </label>
+            <select
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="type"
+              value={type}
+              onChange={(e) => settype(e.target.value)}
+            >
+              <option value="">Select an type level</option>
+              <option value="expense">expense</option>
+              <option value="income">income</option>
+            </select>
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
+              Date
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="date"
+              type="date"
+              placeholder="Enter the date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
+              description
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="description"
+              type="text"
+              placeholder="Enter a description"
+              value={description}
+              onChange={(e) => setdescription(e.target.value)}
+            />
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="amount">
+              Amount
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="amount"
+              type="number"
+              step=".01"
+              placeholder="Enter the amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="categories">
+              categories
+            </label>
+            <select
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="categories"
+              value={categories}
+              onChange={(e) => setcategories(e.target.value)}
+            >
+              <option value="">Select an category level</option>
+              {Array.isArray(categories) && categories.map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="accounts">
+              accounts
+            </label>
+            <select
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="accounts"
+              value={accounts}
+              onChange={(e) => setaccounts(e.target.value)}
+            >
+              <option value="">Select an account level</option>
+              {Array.isArray(accounts) && accounts.map((account) => (
+                <option key={account._id} value={account.name}>
+                  {account.name}
+                </option>
+              ))}
 
-      </select>
+            </select>
+          </div>
+          <div className="w-full md:w-1/6 mb-4 md:mb-0 flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-    <div className="w-full md:w-1/6 mb-4 md:mb-0 flex items-center justify-center">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="submit"
-      >
-        Submit
-      </button>
-    </div>
-  </div>
-  </form>
-</div>
 
 
 
