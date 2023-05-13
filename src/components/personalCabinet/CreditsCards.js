31c1a83e48dc65dc6b502f53054cfe8d52d642d8
vasshorin <)tSz%gBWz1>
2023-05-13 16:57:25 -0700

    Change credit cards design

--- src/components/personalCabinet/CreditsCards.js
+++ src/components/personalCabinet/CreditsCards.js
@@ -18,23 +18,23 @@ const CreditsCards = ({ refreshToken, user }) => {
             setCreditCards(res.data.creditCards);
         }
         handleGetCreditCards();
-      }, [refreshToken]);
-      
+    }, [refreshToken]);
 
-      const handleAddCreditCard = async () => {
+
+    const handleAddCreditCard = async () => {
         console.log("Name " + creditCardName)
         console.log("Balance " + creditCardBalance)
         console.log("Limit " + creditCardLimit)
-    
+
         // Calculate available credit and utilization rate
         const availableCredit = creditCardLimit - creditCardBalance;
         const utilization = (creditCardBalance / creditCardLimit) * 100;
         console.log("Available credit " + availableCredit)
         console.log("Utilization " + utilization)
-    
+
         // Add the new credit card to the db
         const response = await axios.post(`http://localhost:5050/api/newCreditCard`, {
-            name: creditCardName,   
+            name: creditCardName,
             currentBalance: creditCardBalance,
             creditLimit: creditCardLimit,
             availableCredit: availableCredit,
@@ -45,7 +45,7 @@ const CreditsCards = ({ refreshToken, user }) => {
         console.log(response.data);
         setCreditCards([response.data.creditCard, ...creditCards]);
     };
-    
+
     const handleRemoveCreditCard = async (creditCardId) => {
         const res = await axios.delete(`http://localhost:5050/api/deleteCreditCard/${creditCardId}`, {
             headers: {
@@ -58,64 +58,54 @@ const CreditsCards = ({ refreshToken, user }) => {
 
     return (
         <>
-            <div className="flex flex-col md:ml-4">
-                <h2 className="text-xl font-bold mb-4">Credit Card</h2>
-                <div className="flex flex-col md:flex-row mb-4">
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        placeholder="Credit card name"
-                        value={creditCardName}
-                        onChange={(e) => setCreditCardName(e.target.value)}
-                    />
-                    <h3 className="text-m font-bold mb-4">Current balance</h3>
-                    <input
-                        // Enter the balance of the new account
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-4"
-                        type="text"
-                        placeholder="Current balance"
-                        value={creditCardBalance}
-                        onChange={(e) => setCreditCardBalance(e.target.value)}
-                    />
-                    <h3 className="text-m font-bold mb-4">Credit limit</h3>
-                    <input
-                        // Enter the limit of the new account   
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-4"
-                        type="text"
-                        placeholder="Credit limit"
-                        value={creditCardLimit}
-                        onChange={(e) => setCreditCardLimit(e.target.value)}
-                    />
-                    <br />
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 focus:outline-none focus:shadow-outline"
-                        onClick={handleAddCreditCard}
-                    >
-                        Add
-                    </button>
-                </div>
-                <ul>
-                    {creditCards.map(creditCard => (
-                        <li className="mb-2" key={creditCard._id}>
-                            {creditCard.name} ({'$' + creditCard.currentBalance}) {creditCard.creditLimit}
-                            <button
-                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded ml-4 focus:outline-none focus:shadow-outline"
-                                onClick={() => handleRemoveCreditCard(creditCard._id)}
-                            >
-                                Remove
-                            </button>
-                            <button
-                                className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded ml-4 focus:outline-none focus:shadow-outline"
-                                // onClick={() => handleUpdateAccount(account._id)}
-                            >
-                                Update
-                            </button>
+          <div class="flex flex-col md:flex-row items-center mb-6">
+  <h2 class="text-2xl font-bold mr-4 mb-2 md:mb-0">Credit Card</h2>
+  <div class="flex flex-col md:flex-row items-center">
+    <div class="flex items-center mb-2 md:mb-0">
+      <label for="credit-card-name" class="mr-2">Name:</label>
+      <input id="credit-card-name" type="text" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter card name" value={creditCardName} onChange={(e) => setCreditCardName(e.target.value)} />
+    </div>
+    <div class="flex items-center ml-4 mb-2 md:mb-0">
+      <label for="credit-card-balance" class="mr-2">Balance:</label>
+      <input id="credit-card-balance" type="text" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter current balance" value={creditCardBalance} onChange={(e) => setCreditCardBalance(e.target.value)} />
+    </div>
+    <div class="flex items-center ml-4 mb-2 md:mb-0">
+      <label for="credit-card-limit" class="mr-2">Limit:</label>
+      <input id="credit-card-limit" type="text" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter credit limit" value={creditCardLimit} onChange={(e) => setCreditCardLimit(e.target.value)} />
+    </div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 focus:outline-none focus:shadow-outline" onClick={handleAddCreditCard}>Add</button>
+  </div>
+</div>
 
-                        </li>
-                    ))}
-                </ul>
-
-            </div>
+                <table className="w-full border-collapse">
+                    <thead>
+                        <tr className="bg-gray-200">
+                            <th className="px-4 py-2">Credit Card</th>
+                            <th className="px-4 py-2">Balance</th>
+                            <th className="px-4 py-2">Limit</th>
+                            <th className="px-4 py-2">Available Credit</th>
+                            <th className="px-4 py-2">Utilization</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {creditCards.map((creditCard) => (
+                            <tr key={creditCard._id} className="bg-white">
+                                <td className="border px-4 py-2">{creditCard.name}</td>
+                                <td className="border px-4 py-2">{`$${creditCard.currentBalance}`}</td>
+                                <td className="border px-4 py-2">{`$${creditCard.creditLimit}`}</td>
+                                <td className="border px-4 py-2">{`$${creditCard.availableCredit}`}</td>
+                                <td className="border px-4 py-2">{`${creditCard.utilization.toFixed(2)}%`}</td>
+                                    <button
+                                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded ml-4 focus:outline-none focus:shadow-outline"
+                                        onClick={() => handleRemoveCreditCard(creditCard._id)}
+                                    >
+                                        Remove
+                                    </button>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                            
 
         </>
     )
