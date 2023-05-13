38ddd007a58de6d4deec528d3cfb148e30616a5a
vasshorin <)tSz%gBWz1>
2023-05-12 17:39:50 -0700

    Add expense per category summary

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -87,45 +87,71 @@ const Expenses = () => {
     <div>
       <CreateNewExpense onExpenseAdded={onExpenseAdded} />
       {expenses.length > 0 ? (
-        <table className="table-auto mx-auto">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 border">Type</th>
-              <th className="px-4 py-2 border">Amount</th>
-              <th className="px-4 py-2 border">Date</th>
-              <th className="px-4 py-2 border">Description</th>
-              <th className="px-4 py-2 border">Categories</th>
-              <th className="px-4 py-2 border">Accounts</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenses.map((expense, index) => (
-              <tr key={expense._id} className="hover:bg-gray-100">
-                <td className="border px-4 py-2">{expense.type}</td>
-                <td className="border px-4 py-2">${expense.amount.toFixed(2)}</td>
-                <td className="border px-4 py-2">{new Date(expense.date).toLocaleDateString()}</td>
-                <td className="border px-4 py-2">{expense.description}</td>
-                <td className="border px-4 py-2">{expense.categories.join(", ")}</td>
-                <td className="border px-4 py-2">{expense.accountName}</td>
-                  <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded"
-                    onClick={() => {
-                      deleteTransaction(expense._id);
-                    }}
-                  >
-                    Delete
-                  </button>
+        <div>
+          <table className="table-auto mx-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border">Type</th>
+                <th className="px-4 py-2 border">Amount</th>
+                <th className="px-4 py-2 border">Date</th>
+                <th className="px-4 py-2 border">Description</th>
+                <th className="px-4 py-2 border">Categories</th>
+                <th className="px-4 py-2 border">Accounts</th>
               </tr>
-            ))}
-          </tbody>
+            </thead>
+            <tbody>
+              {expenses.map((expense, index) => (
+                <tr key={expense._id} className="hover:bg-gray-100">
+                  <td className="border px-4 py-2">{expense.type}</td>
+                  <td className="border px-4 py-2">${expense.amount.toFixed(2)}</td>
+                  <td className="border px-4 py-2">{new Date(expense.date).toLocaleDateString()}</td>
+                  <td className="border px-4 py-2">{expense.description}</td>
+                  <td className="border px-4 py-2">{expense.categories.join(", ")}</td>
+                  <td className="border px-4 py-2">{expense.accountName}</td>
+                  <td>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded"
+                      onClick={() => {
+                        deleteTransaction(expense._id);
+                      }}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-        </table>
+          <table className="table-auto mx-auto mt-4">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border">Category</th>
+                <th className="px-4 py-2 border">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(
+                expenses.reduce((acc, expense) => {
+                  expense.categories.forEach((category) => {
+                    acc[category] = (acc[category] || 0) + expense.amount;
+                  });
+                  return acc;
+                }, {})
+              ).map(([category, amount]) => (
+                <tr key={category} className="hover:bg-gray-100">
+                  <td className="border px-4 py-2">{category}</td>
+                  <td className="border px-4 py-2">${amount.toFixed(2)}</td>
+                </tr>
+              ))}
+
+            </tbody>
+          </table>
+        </div>
       ) : (
         <p>No expenses found.</p>
       )}
-
     </div>
   );
 };
+
 
 export default Expenses;
