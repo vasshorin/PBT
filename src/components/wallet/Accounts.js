a58fcfdf2bbd215a2587ffbad1577d2f055e3e7c
vasshorin <)tSz%gBWz1>
2023-05-18 12:34:24 -0700

    Change header

--- src/components/wallet/Accounts.js
+++ src/components/wallet/Accounts.js
@@ -78,7 +78,7 @@ const Accounts = ({ refreshToken, user }) => {
   return (
     <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
       <div className="flex justify-between items-center mb-2">
-        <h2 className="text-xl font-bold">Your accounts</h2>
+        <h2 className="text-xl font-bold">Your bank  accounts</h2>
         <div className="flex items-center">
           <button
             className="text-white font-bold py-2 px-4 ml-4 rounded-full mr-2 focus:outline-none focus:shadow-outline"
