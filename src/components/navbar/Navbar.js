f0e6d96ab95918ac1133afc2a96c76159ed38657
vasshorin <)tSz%gBWz1>
2023-05-20 21:17:00 -0700

    Change link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -60,11 +60,11 @@ const Navbar = () => {
       <header>
         <nav className="bg-custom-red-color border-gray-200 px-4 lg:px-6 py-2.5 mb-4 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" className="flex items-center">
+            <Link to="/main" className="flex items-center">
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 PBT
               </span>
-            </a>
+            </Link>
             <div className="flex items-center lg:order-2">
               <div
                 className={`flex items-center ${
